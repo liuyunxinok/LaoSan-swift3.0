274bd8bcea25a2b98f07f77712822ff7d17ccf46
liuyunxin2014 <15890624080@163.com>
2017-09-04 18:16:32 +0800

    String的一点点扩展

--- LaoSan/LSKit/Extensions/NSExtensions/LSStringExtension.swift
+++ LaoSan/LSKit/Extensions/NSExtensions/LSStringExtension.swift
@@ -7,3 +7,36 @@
 //
 
 import Foundation
+
+extension String {
+    
+    /// 验证邮箱格式
+    ///
+    /// - Parameter isStricterFilter: 规定是否严格判断格式
+    /// - Returns: 结果
+    func isValidateEmail(isStricterFilter: Bool) -> Bool {
+        let stricterFilterString = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9-]+[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let laxString = ".+@.+\\.[A-Za-z]{2}[A-Za-z]*"
+        let emailRegex = isStricterFilter ? stricterFilterString : laxString
+        let emailCheck = NSPredicate.init(format: "SELF MATCHES %@", emailRegex)
+        return emailCheck.evaluate(with:self)
+    }
+    
+    
+//    func isValidateIDCard() -> Bool {
+//        if self.characters.count != 18 {
+//            return false
+//        }
+//        let codeArray = ["7","9","10","5","8","4","2","1","6","3","7","9","10","5","8","4","2"]
+//        let checkCodeDic = ["1":"0","0":"1","X":"2","9":"3","8":"4","7":"5","6":"6","5":"7","4":"8","3":"9","2":"10"]
+////        let scan = Scanner(string: self.substring(to: String.Index( 17))
+//    
+//    }
+    
+    
+    
+    
+    
+    
+    
+}
